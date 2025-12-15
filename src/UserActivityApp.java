728210c719668ec8a5451d3094d4bfee85b756b1
boeani05 <flyingdoge@gmx.at>
2025-12-15 17:00:04 +0100

    Add CLI entry point to fetch GitHub user activity
    - Initialize UserActivity from main method
    - Read GitHub username from command line arguments
    - Handle IO, URI, and interruption errors

--- src/UserActivityApp.java
+++ src/UserActivityApp.java
@@ -1,27 +1,18 @@
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Scanner;
 
 public class UserActivityApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         UserActivity userActivity = new UserActivity();
-        String gitHubUserName;
-
-        System.out.println("=== Enter the GitHub-Username you would like to see the activity of ===");
-
-        gitHubUserName = scanner.nextLine();
 
         while (true) {
             try {
-                userActivity.fetchRecentActivity(gitHubUserName);
+                userActivity.fetchRecentActivity(args[0]);
                 break;
             } catch (IOException e) {
                 System.err.println("Something went wrong, please try again!");
-                gitHubUserName = scanner.nextLine();
             } catch (URISyntaxException f) {
                 System.err.println("URI could not be parsed. Please try again!");
-                gitHubUserName = scanner.nextLine();
             } catch (InterruptedException g) {
                 System.err.println("Thread was interrupted unexpectedly!");
             }
